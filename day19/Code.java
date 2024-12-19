cb540b3de2eef3fcb38578ad1d36008e4ff1f865
KaratePikachu <80354180+KaratePikachu@users.noreply.github.com>
2024-12-19 02:27:07 -0500

    pt2

--- day19/Code.java
+++ day19/Code.java
@@ -1,48 +1,82 @@
 import java.util.*;
 import java.io.*;
 
+
+
 public class Code{
-    public static String fileName = "test.txt";
-    public static ArrayList<String> availableTowels = new ArrayList<>();
-    public static ArrayList<String> patterns = new ArrayList<>();
-    public static int numPossible = 0;
-    public static HashMap<Integer,ArrayList<>>
+    public static String fileName = "input.txt";
+    public static ArrayList<char[]> availableTowels = new ArrayList<>();
+    public static ArrayList<char[]> patterns = new ArrayList<>();
+    public static TreeMap<Integer,Long> map = new TreeMap<>();
 
     public static void main(String[] args) throws Exception{
         //Data Structure
 
         Scanner myReader = new Scanner(new File(fileName));
-        availableTowels.addAll(List.of(myReader.nextLine().split(", ")));
+        for(String str : myReader.nextLine().split(", ")){
+            availableTowels.add(str.toCharArray());
+        }
+
         myReader.nextLine();
         while(myReader.hasNextLine()){
-
             String nextLine = myReader.nextLine();
-            patterns.add(nextLine);
-
-            //Read Logic here
-
+            patterns.add(nextLine.toCharArray());
         }
 
-        int numPossible = 0;
-        for(String pattern : patterns){
-            //Get the indexOf for each availtowel in the pattern
-            //feed into LinkedList of sizes
+        long numPossible = 0;
+        for(char[] pattern : patterns){
+            map.clear();
+            //Starting out
+            for(char[] available : availableTowels){
+                if(atIndex(pattern,available,0)){
+                    int nextIndex = available.length;
+                    if(!map.containsKey(nextIndex)){
+                        map.put(nextIndex, 0L);
+                    }
+                    map.put(nextIndex,map.get(nextIndex)+1);
+                }
+            }
+
+            //Push Forward
+            for(int cIndex = 1; cIndex<pattern.length; cIndex++){
+                if(!map.containsKey(cIndex)){
+                    continue;
+                }
+
+                long quant = map.get(cIndex);
+                map.remove(cIndex);
+
+                for(char[] available : availableTowels){
+                    //Starting out
+                    if(atIndex(pattern,available,cIndex)){
+                        int nextIndex = cIndex+available.length;
+                        if(!map.containsKey(nextIndex)){
+                            map.put(nextIndex, 0L);
+                        }
+                        map.put(nextIndex,map.get(nextIndex)+quant);
+                    }
+                }
+                //System.out.println(map.size());
+            }
+            if(map.containsKey(pattern.length)){
+                numPossible+=map.get(pattern.length);
+            }
         }
-
-
         System.out.println(numPossible);
     }
 
-    public static void isPossible(String pattern){
-        boolean foundPossible = false;
-        for(String available : availableTowels){
-            if(pattern.indexOf(available) == 0){
-                if(pattern.length() == available.length()){
-                    Code.numPossible++;
-                    return;
+    public static boolean atIndex(char[] bigger, char[] smaller, int index){
+
+        try{
+            for(int i=0; i<smaller.length; i++){
+                if(bigger[index+i] != smaller[i]){
+                    return false;
                 }
-                isPossible(pattern.substring(available.length()));
             }
+            return true;
+        }
+        catch (IndexOutOfBoundsException e){
+            return false;
         }
     }
 }
